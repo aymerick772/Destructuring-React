843074e456fe2525c908144d5f4e2de513cf2686
Aymerick772 <Aymerick772@gmail.com>
2023-02-12 20:22:30 +0400

    css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Form from "./components/Form"
+import Form2 from "./components/Form2"
 
 function App() {
   return (
     <div className="App">
-      <Form></Form>
+      {/* <Form head={true}></Form> */}
+      <Form2 head={true}></Form2>
+
     </div>
   );
 }
---
+++ src/components/Form2.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+
+class  Form2 extends Component {
+
+    //a faire que si les valeur du tableau ne sont pas dynamique
+
+    render(){
+        return (
+           <div className="">
+                <h1 className="">Commentaire</h1>
+                <button className="btn btn-danger">Valider</button>
+           </div>
+        )
+    }
+}
+
+export default Form2;
