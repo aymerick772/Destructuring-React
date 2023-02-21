450bb26709856b355d20aaddea8f2eb2579789de
Aymerick772 <Aymerick772@gmail.com>
2023-02-21 20:53:52 +0100

    init HOC

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-// import MyComponent from './components/MyComponent';
-import Modal from './components/Modal';
-
-
-
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      showModal:  false,
     }
   }
-  
-  handleShow = () =>{
-    this.setState({
-      showModal: true,
-      }
-    )
-  }
-
-  handleHight = () =>{
-    this.setState({
-      showModal: false,
-    })
-  }
-
   render(){
-    // const modal = this.state.showModal ? ( <Modal/>) : null;
-    //autre méthode
-    const modal = this.state.showModal && ( <Modal closeModal={this.handleHight}/>);
+    
     return (
       <div className="App">
-        {/* <MyComponent/> */}
-        <button onClick={this.handleShow} > afficher modal</button>
-       {modal}
       </div>
     );
   }
