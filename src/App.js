9e8b1a6e22515c489bfd821676dc45113a908055
Aymerick772 <Aymerick772@gmail.com>
2023-02-21 20:51:08 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
 // import MyRef from './components/MyRef'
 import MyRefFunc from './components/MyRefFunc'
-
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      showModal:  false,
-    }
-  }
-  
-  handleShow = () =>{
-    this.setState({
-      showModal: true,
-      }
-    )
+
+    this.refComponent = React.createRef();
   }
 
   handleClick = () => {
@@ -25,22 +15,17 @@ class App extends Component {
     // this.refComponent.current.addFocus();
     this.refComponent.current.focus();
     // console.log('okoke')
-
   }
-
   render(){
-    // const modal = this.state.showModal ? ( <Modal/>) : null;
-    //autre méthode
-    const modal = this.state.showModal && ( <Modal closeModal={this.handleHight}/>);
     return (
       <div className="App">
-
         {/* <MyRef ref={this.refComponent}/> */}
         <MyRefFunc name="okok" ref={this.refComponent}/>
+        
         <button onClick={this.handleClick}>Valider</button>
       </div>
     );
   }
 }
-//
+
 export default App;
